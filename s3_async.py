fc4cf0c51983320a38d87c25722ab28492bec2bc
Giulio Calzolari <giulio.calzolari@cloudreach.co.uk>
2015-06-01 11:04:30 +0200

    up

--- s3_async.py
+++ s3_async.py
@@ -62,9 +62,9 @@ class ChangeHandler(FileSystemEventHandler):
         if not os.path.isfile(key_file): 
             f = open(key_file,'w+')
             f.write("[default]"+"\n")
-            f.write("accessKeyId:"+  self.current_repl["acces_key"]+"\n")
-            f.write("secretKey:"+  self.current_repl["secret_key"]+"\n")
-            f.write("region:"+  self.current_repl["region"]+"\n")
+            f.write("aws_access_key_id="+  self.current_repl["acces_key"]+"\n")
+            f.write("aws_secret_access_key="+  self.current_repl["secret_key"]+"\n")
+            f.write("region="+  self.current_repl["region"]+"\n")
             f.close()
 
         os.environ["AWS_CONFIG_FILE"] = key_file
